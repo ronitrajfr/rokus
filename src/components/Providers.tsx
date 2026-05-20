15c16660a20f676f3f3138bc0666df8a4fdb07d3
ronitrajfr <ronitrajofficial7@gmail.com>
2026-05-20 14:15:16 +0530

    feat: initial commit for rokus

---
+++ src/components/Providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+// interface Props {
+//   children: ReactNode;
+// }
+
+const Providers = ({ children, session }: any) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+
+export default Providers;
